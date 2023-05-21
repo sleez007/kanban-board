f129773a281d0e9a21386a4da6dd51a15b404938
Kingsley Etoka <etokakingsley@gmail.com>
2023-05-21 01:07:52 +0100

    url fix

--- apps/shell/webpack.prod.config.js
+++ apps/shell/webpack.prod.config.js
@@ -2,7 +2,7 @@ const { withModuleFederation } = require('@nx/angular/module-federation');
 const config = require('./module-federation.config');
 module.exports = withModuleFederation({
   ...config,
-  remotes: [['board', 'https://kanban-board-inky.vercel.app/board']],
+  remotes: [['board', 'https://main.d13r90l17357pu.amplifyapp.com/board']],
   /*
    * Remote overrides for production.
    * Each entry is a pair of a unique name and the URL where it is deployed.
